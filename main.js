7e1e2b72b360f6a080773156057192eb54c4ee5f
Sola Kabuta <solakabuta@gmail.com>
2026-07-06 01:09:40 +0200

    feat: implement dynamic charts display

--- main.js
+++ main.js
@@ -6,24 +6,42 @@ let total = document.getElementById("total");
 let pourcentage = document.getElementById("pourcentage");
 
 // DAYS
-let monday = document.getElementById("monday")
+// let monday = document.getElementById("monday")
 
-// DAY AMOUNT
-let mondayAmount = document.getElementById("mon-amount")
+// // DAY AMOUNT
+// let mondayAmount = document.getElementById("mon-amount")
 
 // DISPLAY HOVER AMOUNT
-monday.addEventListener("mouseenter", function() {
-    mondayAmount.classList.toggle("invisible");
-})
+// monday.addEventListener("mouseenter", function() {
+//     mondayAmount.classList.toggle("invisible");
+// })
 
 
 fetch("/data.json")
   .then(response => response.json())
   .then(data => {
-    console.log(data);
 
     total = data.reduce((sum, item) => sum + item.amount, 0)
     console.log(total)
+
+    data.map(displayDay)
+
+    function displayDay(item) {
+      console.log(item.day)
+      charts.innerHTML += `<li id="monday">
+          <div id="mon-amount" class="invisible bg-[#3A2315] p-2  text-white text-center rounded-lg">
+             <p>${item.amount}</p> 
+          </div>
+          <div class="bg-[#EC755D] w-10 h-20 rounded-md relative">
+              <span class="absolute -top-6 left-1/2 -translate-x-1/2 text-sm opacity-0">
+                  $17.45
+              </span>
+          </div>
+          <p class="opacity-60">${item.day}</p>
+      </li>`
+    }
+
+    
 
     
 });
