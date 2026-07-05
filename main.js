2a092ffe633ce6fd8104e7919a8606cca1709366
Sola Kabuta <solakabuta@gmail.com>
2026-07-06 00:43:59 +0200

    style: font-family DM Sans change

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 
+let balance = document.getElementById("balance").innerHTML = "$" + 921.48; 
 
+let charts = document.getElementById("charts"); 
 let total = document.getElementById("total");
 let pourcentage = document.getElementById("pourcentage");
 
@@ -9,18 +11,24 @@ let monday = document.getElementById("monday")
 // DAY AMOUNT
 let mondayAmount = document.getElementById("mon-amount")
 
-// DISPLAY AMOUNT
-monday.addEventListener("mouseenter", displayMonday)
+// DISPLAY HOVER AMOUNT
+monday.addEventListener("mouseenter", function() {
+    mondayAmount.classList.toggle("invisible");
+})
 
-function displayMonday() {
-  mondayAmount.classList.toggle("invisible")
-}
 
 fetch("/data.json")
   .then(response => response.json())
   .then(data => {
     console.log(data);
+
+    total = data.reduce((sum, item) => sum + item.amount, 0)
+    console.log(total)
+
+    
 });
 
+// WHAT DAY R WE 
+let today = new Date().getDay();
 
 // TOTAL
